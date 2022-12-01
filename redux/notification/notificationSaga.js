aa7ac0b081606334808f240c80a20f8bf22470ee
Enes Malik Ozer <enes@altogic.com>
2022-12-01 15:57:11 +0300

    realtime notification for invite team

---
+++ redux/notification/notificationSaga.js
@@ -0,0 +1,52 @@
+import { call, put, takeEvery, all } from 'redux-saga/effects';
+import notificationService from '@/services/notification';
+import { notificationActions } from './notificationSlice';
+
+function* getNotifications(action) {
+  try {
+    const { data, errors } = yield call(notificationService.getNotifications, action.payload);
+
+    if (errors) {
+      throw errors;
+    }
+    yield put(notificationActions.getNotificationsSuccess(data));
+  } catch (error) {
+    yield put(notificationActions.getNotificationsFailure(error));
+  }
+}
+
+function* markNotificationAsRead(action) {
+  try {
+    const { data, errors } = yield call(notificationService.markNotificationAsRead, action.payload);
+    if (errors) {
+      throw errors;
+    }
+    yield put(notificationActions.markNotificationAsReadSuccess(data));
+  } catch (error) {
+    yield put(notificationActions.markNotificationAsReadFailure(error));
+  }
+}
+
+function* sendNotification({ payload }) {
+  try {
+    const { data, errors } = yield call(notificationService.sendNotification, payload);
+    if (errors) {
+      throw errors;
+    }
+    yield put(notificationActions.sendNotificationSuccess(data));
+  } catch (error) {
+    yield put(notificationActions.sendNotificationFailure(error));
+  }
+}
+function* receiveNotificationRealtime(action) {
+  yield put(notificationActions.receiveNotificationRealtimeSuccess(action.payload));
+}
+
+export default function* notificationSaga() {
+  yield all([
+    takeEvery(notificationActions.getNotifications.type, getNotifications),
+    takeEvery(notificationActions.markNotificationAsRead.type, markNotificationAsRead),
+    takeEvery(notificationActions.sendNotification.type, sendNotification),
+    takeEvery(notificationActions.receiveNotificationRealtime, receiveNotificationRealtime)
+  ]);
+}
--- redux/rootSaga.js
+++ redux/rootSaga.js
@@ -4,9 +4,16 @@ import authSaga from './auth/authSaga';
 import topicSaga from './topics/topicSaga';
 import companySaga from './company/companySaga';
 import fileSaga from './file/fileSaga';
+import notificationSaga from './notification/notificationSaga';
 
 function* rootSaga() {
-  yield all([fork(authSaga), fork(topicSaga), fork(companySaga), fork(fileSaga)]);
+  yield all([
+    fork(authSaga),
+    fork(topicSaga),
+    fork(companySaga),
+    fork(fileSaga),
+    fork(notificationSaga)
+  ]);
 }
 
 export default rootSaga;
