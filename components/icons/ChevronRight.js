66779b0c66f9a3874a2c4b65c09bfe9b5069a7c7
Halit Güvenilir <halitguvenilir@gmail.com>
2022-12-06 10:08:30 +0300

    Icons updated

---
+++ components/icons/ChevronRight.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+const SvgChevronRight = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <path
+      d="m7.5 15 5-5-5-5"
+      stroke="currentColor"
+      strokeWidth={1.667}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default SvgChevronRight;
