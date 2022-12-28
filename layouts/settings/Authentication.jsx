410b402b28e1b429c0ecdc7ac5248847d1609289
Halit <halitguvenilir@gmail.com>
2022-12-28 14:08:22 +0300

    Authentication list box updated theme

--- layouts/settings/Authentication.jsx
+++ layouts/settings/Authentication.jsx
@@ -35,22 +35,28 @@ export default function Authentication() {
         return (
           <>
             Users must sign up to your app with{' '}
-            <strong className="text-slate-700">name, email and password.</strong> Once authenticated
-            they will be able to add Ideas, comments and vote.
+            <strong className="text-slate-700 dark:text-aa-400 purple:text-pt-400">
+              name, email and password.
+            </strong>{' '}
+            Once authenticated they will be able to add Ideas, comments and vote.
           </>
         );
       case 'Guest Authentication':
         return (
           <>
             Users can add Ideas, comments and vote, without signing up. Guests must leave a{' '}
-            <strong className="text-slate-700">name and email.</strong>
+            <strong className="text-slate-700 dark:text-aa-400 purple:text-pt-400">
+              name and email.
+            </strong>
           </>
         );
       case 'Anonymous':
         return (
           <>
             Users can add Ideas, comments and vote, without signing up. Guests are{' '}
-            <strong className="text-slate-700">not required to leave any details.</strong>
+            <strong className="text-slate-700 dark:text-aa-400 purple:text-pt-400">
+              not required to leave any details.
+            </strong>
           </>
         );
       case 'Custom':
@@ -95,13 +101,13 @@ export default function Authentication() {
                   updateCompanyAuthentication('type', selected);
                 }}>
                 <div className="relative">
-                  <Listbox.Button className="relative w-full md:w-[220px] inline-flex bg-white py-3.5 px-[14px] border border-slate-300 rounded-lg text-left cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                    <span className="block text-gray-500 text-sm tracking-sm truncate">
+                  <Listbox.Button className="relative w-full md:w-[220px] inline-flex bg-white dark:bg-aa-800 purple:bg-pt-800 py-3.5 px-[14px] border border-slate-300 dark:border-aa-400 purple:border-pt-400 rounded-lg text-left cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block text-gray-500 dark:text-aa-200 purple:text-pt-200 text-sm tracking-sm truncate">
                       {authenticationSelected}
                     </span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3.5">
                       <svg
-                        className="w-5 h-5 text-gray-500"
+                        className="w-5 h-5 text-gray-500 dark:text-aa-200 purple:text-pt-200"
                         viewBox="0 0 20 20"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg">
@@ -120,13 +126,15 @@ export default function Authentication() {
                     leave="transition ease-in duration-100"
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0">
-                    <Listbox.Options className="absolute mt-1 max-h-60 w-full md:w-[220px] overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50">
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full md:w-[220px] overflow-auto rounded-md bg-white dark:bg-aa-800 purple:bg-pt-900 py-1 text-base shadow-lg focus:outline-none sm:text-sm z-50">
                       {AUTHENTICATION_METHOD.map((item) => (
                         <Listbox.Option
                           key={item}
                           className={({ active }) =>
-                            `relative flex items-center justify-between cursor-default select-none py-2 px-3.5 ${
-                              active ? 'bg-slate-100' : 'text-slate-900'
+                            `relative flex items-center justify-between cursor-default select-none py-2 px-3.5 transition hover:text-slate-900 dark:hover:text-aa-100 purple:hover:text-pt-100 ${
+                              active
+                                ? 'bg-slate-100 dark:bg-aa-700 purple:bg-pt-700'
+                                : 'text-slate-900 dark:text-aa-200 purple:text-pt-200'
                             }`
                           }
                           value={item}>
@@ -134,12 +142,14 @@ export default function Authentication() {
                             <>
                               <span
                                 className={`block truncate ${
-                                  selected ? 'font-medium' : 'font-normal'
+                                  selected
+                                    ? 'text-slate-900 dark:text-aa-100 purple:text-pt-100'
+                                    : 'font-normal'
                                 }`}>
                                 {item}
                               </span>
                               {selected ? (
-                                <span className="flex items-center pl-3 text-indigo-700">
+                                <span className="flex items-center pl-3 text-indigo-700 dark:text-aa-200 purple:text-pt-200">
                                   <svg
                                     className="w-5 h-5"
                                     viewBox="0 0 20 20"
@@ -166,7 +176,7 @@ export default function Authentication() {
             </div>
           </div>
           <div className="py-6">
-            <div className="bg-slate-100 text-slate-500 py-6 px-8 text-sm tracking-sm rounded-lg">
+            <div className="bg-slate-100 dark:bg-aa-600 purple:bg-pt-800 text-slate-500 dark:text-aa-100 purple:text-pt-100 py-6 px-8 text-sm tracking-sm rounded-lg">
               {setAuthenticationDescription(authenticationSelected)}
             </div>
           </div>
