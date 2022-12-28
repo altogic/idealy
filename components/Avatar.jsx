878f2020220d4abf681d18866d0f49d508e65ada
Halit <halitguvenilir@gmail.com>
2022-12-28 16:34:49 +0300

    Updated

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -8,7 +8,7 @@ export default function Avatar({ src, alt, size }) {
     <div>
       {name && (
         <div
-          className={`rounded-full bg-slate-100 flex items-center justify-center ${
+          className={`rounded-full bg-slate-100 dark:bg-aa-300 purple:bg-pt-300 flex items-center justify-center ${
             size ?? 'w-11 h-11'
           }`}>
           <span className="text-slate-700 text-lg tracking-sm">{name[0]?.charAt(0)}</span>
--- components/CompanyAvatar.jsx
+++ components/CompanyAvatar.jsx
@@ -9,7 +9,7 @@ export default function CompanyAvatar({ logoUrl, name, className, size }) {
       ) : (
         <div>
           <div
-            className={`inline-flex items-center justify-center bg-white text-slate-800 tracking-md border-2 border-slate-100 rounded-lg group-hover:border-indigo-700 ${className} ${size} `}>
+            className={`inline-flex items-center justify-center bg-white dark:bg-aa-300 purple:bg-pt-300 text-slate-800 tracking-md border-2 border-slate-100 dark:border-aa-200 purple:border-pt-200 rounded-lg group-hover:border-indigo-700 ${className} ${size} `}>
             {name[0]?.charAt(0)}
             {name[1]?.charAt(0)}
           </div>
