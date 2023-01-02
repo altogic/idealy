da15f0bfed22ba60597273e9e9a27d0ff25ce1de
Halit Güvenilir <halitguvenilir@gmail.com>
2023-01-02 15:36:07 +0300

    Icons updated

---
+++ components/icons/ChevronUp.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+const SvgChevronUp = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    width="1em"
+    height="1em"
+    {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+  </svg>
+);
+export default SvgChevronUp;
