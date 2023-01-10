891bc6bf7c25ae58700fd9378087b2450b128157
Halit <halitguvenilir@gmail.com>
2023-01-10 11:11:52 +0300

    Idea action button component text color problem fixed

--- components/Idea/admin/IdeaActionButton.jsx
+++ components/Idea/admin/IdeaActionButton.jsx
@@ -5,9 +5,7 @@ export default function IdeaActionButton({ Icon, className, onClick, type }) {
       className="w-8 h-8 flex justify-center items-center"
       onClick={onClick}
       title={type}>
-      <Icon
-        className={`w-4 h-4 ${className || 'text-slate-600 dark:text-aa-300 purple:text-pt-300'}`}
-      />
+      <Icon className={`w-4 h-4 text-slate-600 dark:text-aa-300 purple:text-pt-300 ${className}`} />
     </button>
   );
 }
