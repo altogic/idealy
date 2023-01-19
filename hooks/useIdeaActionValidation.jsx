d63f8010e2f344929aafc50988b586ad3bf4646c
Enes Malik Ozer <enes@altogic.com>
2023-01-19 04:06:55 +0300

    moderator role added

--- hooks/useIdeaActionValidation.jsx
+++ hooks/useIdeaActionValidation.jsx
@@ -4,9 +4,5 @@ export default function useIdeaActionValidation(model) {
   const company = useSelector((state) => state.company.company);
   const user = useSelector((state) => state.auth.user);
   const userIp = useSelector((state) => state.auth.userIp);
-  return (
-    userIp === model?.ip ||
-    user?._id === model?.author?._id ||
-    (user && (company?.role === 'Owner' || company?.role === 'Admin'))
-  );
+  return userIp === model?.ip || user?._id === model?.author?._id || (user && company?.role);
 }
