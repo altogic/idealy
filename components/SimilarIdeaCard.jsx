66e0a47c711243eada588c2d15526a56d49c5ba6
Halit <halitguvenilir@gmail.com>
2023-01-22 14:02:55 +0300

    Similar idea card updated theme

--- components/SimilarIdeaCard.jsx
+++ components/SimilarIdeaCard.jsx
@@ -12,7 +12,7 @@ export default function SimilarIdeaCard({ idea }) {
   return (
     <button
       type="button"
-      className="w-full px-2 py-6 lg:p-6 rounded-lg transition hover:bg-slate-200"
+      className="w-full px-2 py-6 lg:p-6 rounded-lg transition hover:bg-slate-50 dark:hover:bg-aa-800 purple:hover:bg-pt-900"
       onClick={() => {
         router.push(
           {
@@ -30,18 +30,18 @@ export default function SimilarIdeaCard({ idea }) {
         dispatch(toggleFeedBackDetailModal());
       }}>
       <div className="flex items-start lg:items-center gap-6">
-        <div className="flex flex-col items-center justify-center bg-white px-3 md:px-5 border rounded-lg border-gray-400">
-          <span className="text-indigo-700 text-xl font-semibold tracking-md">
+        <div className="flex flex-col items-center justify-center bg-white dark:bg-aa-50 dark:bg-opacity-20 purple:bg-opacity-20 py-2 px-3 border border-gray-300 dark:border-aa-600 purple:border-pt-800 rounded-lg">
+          <span className="text-indigo-700 dark:text-aa-200 purple:text-pt-200 text-xl font-semibold tracking-md">
             {idea?.voteCount}
           </span>
-          <span className="text-slate-500 text-sm tracking-sm">
+          <span className="text-slate-500 dark:text-aa-300 purple:text-pt-300 text-sm tracking-sm">
             {idea?.voteCount <= 1 ? 'Vote' : 'Votes'}
           </span>
         </div>
         <div className="w-full">
           <div className="flex items-center gap-2 mb-1">
             <h3
-              className="max-w-[700px] text-slate-800 text-xl font-semibold tracking-md text-left truncate"
+              className="max-w-[700px] text-slate-800 dark:text-aa-200 purple:text-pt-200 text-xl font-semibold tracking-md text-left truncate"
               title={idea?.title}>
               {idea?.title}
             </h3>
@@ -55,7 +55,7 @@ export default function SimilarIdeaCard({ idea }) {
             <div className="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-3">
               <div className="flex items-center gap-3">
                 {/* User */}
-                <span className="text-slate-700 text-sm font-medium tracking-sm">
+                <span className="text-slate-700 dark:text-aa-200 purple:text-pt-200 text-sm font-medium tracking-sm">
                   {idea?.author
                     ? idea?.author.name
                     : idea?.guestName
@@ -66,7 +66,7 @@ export default function SimilarIdeaCard({ idea }) {
                   <circle cx={4} cy={4} r={3} />
                 </svg>
                 {/* Date */}
-                <span className="text-slate-500 text-sm tracking-sm">
+                <span className="text-slate-500 dark:text-aa-300 purple:text-pt-300 text-sm tracking-sm">
                   {DateTime.fromISO(idea?.createdAt).setLocale('en').toRelative()}
                 </span>
               </div>
