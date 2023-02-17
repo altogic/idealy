d41afcbe762aa35282a7ffff144f8609b640e671
Enes Malik Ozer <enes@altogic.com>
2023-02-17 16:59:22 +0300

    user card interaction area

--- components/Idea/IdeaInfo.jsx
+++ components/Idea/IdeaInfo.jsx
@@ -22,34 +22,46 @@ export default function IdeaInfo({ idea, detail }) {
     };
   }, [feedBackDetailModal]);
   return (
-    <button
-      className="flex items-center gap-4"
-      type="button"
-      onClick={(e) => {
-        e.stopPropagation();
-        const userCards = document.querySelectorAll('.idea-user-card');
-        userCards.forEach((userCard) => {
-          // eslint-disable-next-line no-param-reassign
-          userCard.style.display = 'none';
-        });
-        setUserCardStyle({
-          top: detail ? '-6rem' : '3rem',
-          left: detail ? '-6rem' : '2.5rem',
-          display: 'flex'
-        });
-      }}>
-      {/* Author */}
-      <div className="flex items-center gap-2">
-        <Avatar
-          src={idea?.author?.profilePicture || idea?.guestAvatar}
-          alt={idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
-          size="w-7 h-7"
-          fontSize="text-xs"
+    <div className="flex items-center gap-4">
+      <button
+        className="flex items-center gap-4"
+        type="button"
+        onClick={(e) => {
+          e.stopPropagation();
+          const userCards = document.querySelectorAll('.idea-user-card');
+          userCards.forEach((userCard) => {
+            // eslint-disable-next-line no-param-reassign
+            userCard.style.display = 'none';
+          });
+          setUserCardStyle({
+            top: detail ? '-6rem' : '3rem',
+            left: detail ? '-6rem' : '2.5rem',
+            display: 'flex'
+          });
+        }}>
+        {/* Author */}
+        <div className="flex items-center gap-2">
+          <Avatar
+            src={idea?.author?.profilePicture || idea?.guestAvatar}
+            alt={idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
+            size="w-7 h-7"
+            fontSize="text-xs"
+          />
+          <span className="text-slate-700 dark:text-aa-200 purple:text-pt-200 text-sm font-medium tracking-sm">
+            {idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
+          </span>
+        </div>
+
+        <UserCard
+          id="idea-user-card"
+          profilePicture={idea?.author?.profilePicture || idea?.guestAvatar}
+          name={idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
+          email={
+            idea?.author ? idea?.author.email : idea?.guestEmail ? idea?.guestEmail : idea?.email
+          }
+          style={userCardStyle}
         />
-        <span className="text-slate-700 dark:text-aa-200 purple:text-pt-200 text-sm font-medium tracking-sm">
-          {idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
-        </span>
-      </div>
+      </button>
       <svg
         className="hidden lg:block h-1 w-1 text-slate-500 dark:text-aa-400 purple:text-pt-400"
         fill="currentColor"
@@ -60,15 +72,6 @@ export default function IdeaInfo({ idea, detail }) {
       <span className="text-slate-500 dark:text-aa-300 purple:text-pt-300 text-sm tracking-sm">
         {DateTime.fromISO(idea?.createdAt).setLocale('en').toRelative()}
       </span>
-      <UserCard
-        id="idea-user-card"
-        profilePicture={idea?.author?.profilePicture || idea?.guestAvatar}
-        name={idea?.author ? idea?.author.name : idea?.guestName ? idea?.guestName : idea?.name}
-        email={
-          idea?.author ? idea?.author.email : idea?.guestEmail ? idea?.guestEmail : idea?.email
-        }
-        style={userCardStyle}
-      />
-    </button>
+    </div>
   );
 }
