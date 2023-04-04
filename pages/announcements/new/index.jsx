4adcd9f522fc46980dba41a45e36a630e9e513a4
Enes Malik Ozer <enes@altogic.com>
2023-04-04 17:43:09 +0300

    🐛 fix(new-announcement): debounce onSuccess callback to prevent multiple redirects ✨ feat(new-announcement): add lodash library to handle debouncing of onSuccess callback

--- pages/announcements/new/index.jsx
+++ pages/announcements/new/index.jsx
@@ -4,18 +4,21 @@ import AnnouncementForm from '@/components/AnnouncementForm';
 import { announcementActions } from '@/redux/announcement/announcementSlice';
 import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
+import _ from 'lodash';
 
 export default function NewAnnouncement() {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const onSuccess = _.debounce((data) => {
+    router.push(`/announcements/edit/${data.slug}`);
+  }, 500);
+
   const handleSaveAnnouncements = (req) => {
     dispatch(
       announcementActions.createAnnouncement({
         ...req,
-        onSuccess: (data) => {
-          router.push(`/announcements/edit/${data.slug}`);
-        }
+        onSuccess: (data) => onSuccess(data)
       })
     );
   };
