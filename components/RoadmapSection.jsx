d0135eaf149035054b252aa22d142058a307e429
Enes Malik Ozer <enes@altogic.com>
2023-04-07 02:23:48 +0300

    🎨 style(RoadmapSection.jsx): improve layout and add border to status count 🐛 fix(RoadmapSection.jsx): disable button when status name is empty

--- components/RoadmapSection.jsx
+++ components/RoadmapSection.jsx
@@ -4,6 +4,7 @@ import { useMemo, useState } from 'react';
 import { Draggable, resetServerContext } from 'react-beautiful-dnd';
 import { useDispatch, useSelector } from 'react-redux';
 import { realtime } from '@/utils/altogic';
+import cn from 'classnames';
 import EmptyState from './EmptyState';
 import { Eye, EyeSlash } from './icons';
 import RoadMapCard from './RoadMapCard';
@@ -45,44 +46,52 @@ export default function RoadmapSection({ status, ideas, provided, roadmap, ...re
       {...provided.droppableProps}
       {...rest}>
       <div className="flex items-center justify-between gap-2 pb-4 border-b border-slate-200 dark:border-aa-600 purple:border-pt-800 mb-4">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 flex-1">
           <svg className="h-2.5 w-2.5" fill={status?.color} viewBox="0 0 8 8">
             <circle cx={4} cy={4} r={3} />
           </svg>
-          <span className="text-slate-700 dark:text-aa-200 purple:text-pt-200 text-base font-medium tracking-sm">
+
+          <div className="text-slate-700 dark:text-aa-200 purple:text-pt-200 text-base font-medium tracking-sm flex items-center justify-between flex-1">
             {status?.name || 'No Status'}{' '}
-            <span className="font-normal">({ideas?.length || 0})</span>
-          </span>
+            <div className="rounded-md border border-slate-200 dark:border-aa-1000 purple:border-pt-1000 px-2 bg-slate-50 dark:bg-aa-600 purple:bg-pt-800 ">
+              <span className="font-normal">{ideas?.length || 0}</span>
+            </div>
+          </div>
         </div>
         <div className="relative group border-l border-slate-200 dark:border-aa-600 purple:border-pt-800 px-4">
           {isGuest ? (
             <RoadmapVisibilityIcon isPrivate={isPrivate} />
           ) : (
             <Tooltip>
-              <TooltipTrigger
-                onClick={() => {
-                  const publicStatuses = structuredClone(roadmap?.publicStatuses || []);
-                  if (isPrivate) {
-                    publicStatuses.splice(publicStatuses.indexOf(status._id), 1);
-                  } else {
-                    publicStatuses.push(status._id);
-                    realtime.send(company._id, 'make-status-public', {
-                      statusId: status._id,
-                      roadmapId: roadmap._id,
-                      data: sortedIdeas,
-                      sender: user?.id
-                    });
-                  }
-                  dispatch(
-                    companyActions.updateCompanySubLists({
-                      id: roadmap._id,
-                      property: 'roadmaps',
-                      update: { publicStatuses },
-                      role: company?.role
-                    })
-                  );
-                }}>
-                <RoadmapVisibilityIcon isPrivate={isPrivate} />
+              <TooltipTrigger asChild>
+                <button
+                  type="button"
+                  onClick={() => {
+                    const publicStatuses = structuredClone(roadmap?.publicStatuses || []);
+                    if (isPrivate) {
+                      publicStatuses.splice(publicStatuses.indexOf(status._id), 1);
+                    } else {
+                      publicStatuses.push(status._id);
+                      realtime.send(company._id, 'make-status-public', {
+                        statusId: status._id,
+                        roadmapId: roadmap._id,
+                        data: sortedIdeas,
+                        sender: user?.id
+                      });
+                    }
+                    dispatch(
+                      companyActions.updateCompanySubLists({
+                        id: roadmap._id,
+                        property: 'roadmaps',
+                        update: { publicStatuses },
+                        role: company?.role
+                      })
+                    );
+                  }}
+                  disabled={!status?.name}
+                  className={cn(!status?.name && 'cursor-not-allowed')}>
+                  <RoadmapVisibilityIcon isPrivate={isPrivate} />
+                </button>
               </TooltipTrigger>
               <TooltipContent>{isPrivate ? 'Hide Status' : 'Show Status'}</TooltipContent>
             </Tooltip>
