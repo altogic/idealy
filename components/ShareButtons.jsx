2b7c65aaab112109401a24b7749c52b9e62c1406
Enes Malik Ozer <enes@altogic.com>
2023-04-11 16:02:31 +0300

    🎨 style(ShareButtons.jsx): improve ShareButtons component UI by adding hover effects and rounded corners 🐛 fix(ShareButtons.jsx): fix TwitterShareButton and LinkedinShareButton not being clickable by wrapping them in a div ✨ feat(ShareButtons.jsx): add hover effects and fill colors to the copyToClipboard button

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -12,16 +12,26 @@ export default function ShareButtons({ url, title, summary, hashtags }) {
   return (
     <div className="flex gap-4 items-center ">
       <FacebookShareButton url={url} quote={summary} hashtag={hashtags}>
-        <Facebook className="w-5 h-5 text-slate-500 dark:text-aa-200 purple:text-pt-200 ml-2 " />
+        <div className="w-8 h-8 flex justify-center items-center rounded-full duration-300 hover:text-white hover:bg-[#1877F2] dark:hover:bg-gray-700 dark:hover:text-[#1877F2] purple:hover:bg-gray-700 purple:hover:text-[#1877F2] text-gray-700 dark:text-aa-200 purple:text-pt-200">
+          <Facebook className="w-5 h-5" />
+        </div>
       </FacebookShareButton>
-      <TwitterShareButton url={url} title={title} hashtags={hashtags}>
-        <Twitter className="w-5 h-5 text-slate-500 dark:text-aa-200 purple:text-pt-200 " />
-      </TwitterShareButton>
-      <LinkedinShareButton url={url} title={title} summary={summary} source={url}>
-        <LinkedIn className="w-5 h-5 text-slate-500 dark:text-aa-200 purple:text-pt-200 " />
-      </LinkedinShareButton>
-      <button type="button" onClick={copyToClipboard}>
-        <LinkSimple className="w-5 h-5 fill-slate-500 dark:fill-aa-200 purple:fill-pt-200 " />
+      <div className="w-8 h-8 flex justify-center items-center rounded-full duration-300 hover:text-white hover:bg-[#1DA1F2] dark:hover:bg-gray-700 dark:hover:text-[#1DA1F2] purple:hover:bg-gray-700 purple:hover:text-[#1DA1F2] text-gray-700 dark:text-aa-200 purple:text-pt-200">
+        <TwitterShareButton url={url} title={title} hashtags={hashtags}>
+          <Twitter className="w-5 h-5" />
+        </TwitterShareButton>
+      </div>
+      <div className="w-8 h-8 flex justify-center items-center rounded-full duration-300 hover:text-white hover:bg-[#0A66C2] dark:hover:bg-gray-700 dark:hover:text-[#0A66C2] purple:hover:bg-gray-700 purple:hover:text-[#0A66C2] text-gray-700 dark:text-aa-200 purple:text-pt-200">
+        <LinkedinShareButton url={url} title={title} summary={summary} source={url}>
+          <LinkedIn className="w-5 h-5" />
+        </LinkedinShareButton>
+      </div>
+
+      <button
+        className="w-8 h-8 flex justify-center items-center rounded-full group duration-300 hover:bg-[#8A2BE2] dark:hover:bg-gray-700  purple:hover:bg-gray-700"
+        type="button"
+        onClick={copyToClipboard}>
+        <LinkSimple className="w-5 h-5 group-hover:fill-white group-hover:dark:fill-[#8A2BE2] group-hover:purple:fill-[#8A2BE2] fill-gray-700 dark:fill-aa-200 purple:fill-pt-200" />
       </button>
     </div>
   );
