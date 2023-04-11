d129b57dae003e380722cc5ed0e79882f50b0e9d
Enes Malik Ozer <enes@altogic.com>
2023-04-11 16:53:59 +0300

    🎨 style(ShareButtons.jsx): decrease gap between share buttons from 4 to 2

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -10,7 +10,7 @@ export default function ShareButtons({ url, title, summary, hashtags }) {
     ToastMessage.success('Copied to clipboard');
   }
   return (
-    <div className="flex gap-4 items-center ">
+    <div className="flex gap-2 items-center ">
       <FacebookShareButton url={url} quote={summary} hashtag={hashtags}>
         <div className="w-8 h-8 flex justify-center items-center rounded-full duration-300 hover:text-white hover:bg-[#1877F2] dark:hover:bg-gray-700 dark:hover:text-[#1877F2] purple:hover:bg-gray-700 purple:hover:text-[#1877F2] text-gray-700 dark:text-aa-200 purple:text-pt-200">
           <Facebook className="w-5 h-5" />
