f619405f7033348e9486af9a436437356745e172
Enes Malik Ozer <enes@altogic.com>
2023-04-14 15:03:22 +0300

    🎨 style(roadmaps.jsx): adjust layout of BaseListBox and SearchInput components

--- pages/roadmaps.jsx
+++ pages/roadmaps.jsx
@@ -324,38 +324,38 @@ export default function RoadMapAdmin() {
                             </TooltipContent>
                           </Tooltip>
                         )}
-
-                        <BaseListBox
-                          value={roadmap}
-                          label={roadmap?.name}
-                          field="name"
-                          options={filteredRoadmaps}
-                          size="xxl"
-                          className="flex-1"
-                          onChange={(value) => {
-                            setRoadmap(value);
-                            dispatch(ideaActions.setSelectedIdea(value));
-                            router.push({
-                              pathname: '/roadmaps',
-                              query: { roadmap: value._id }
-                            });
-                          }}
-                          type="create">
-                          <button
-                            type="button"
-                            className="inline-flex items-center gap-3 text-slate-400 py-2"
-                            onClick={() => setIsCreate(!isCreate)}>
-                            <Plus className="w-4 h-4 text-slate-500 dark:text-aa-200 purple:text-pt-200" />
-                            Add a new roadmap
-                          </button>
-                        </BaseListBox>
-                        <SearchInput
-                          searchText={searchText}
-                          onSearch={(e) => onSearchChange(e)}
-                          onClear={() => {
-                            setSearchText('');
-                          }}
-                        />
+                        <div className="flex flex-1 justify-between items-center">
+                          <BaseListBox
+                            value={roadmap}
+                            label={roadmap?.name}
+                            field="name"
+                            options={filteredRoadmaps}
+                            size="xs"
+                            onChange={(value) => {
+                              setRoadmap(value);
+                              dispatch(ideaActions.setSelectedIdea(value));
+                              router.push({
+                                pathname: '/roadmaps',
+                                query: { roadmap: value._id }
+                              });
+                            }}
+                            type="create">
+                            <button
+                              type="button"
+                              className="inline-flex items-center gap-3 text-slate-400 py-2"
+                              onClick={() => setIsCreate(!isCreate)}>
+                              <Plus className="w-4 h-4 text-slate-500 dark:text-aa-200 purple:text-pt-200" />
+                              Add a new roadmap
+                            </button>
+                          </BaseListBox>
+                          <SearchInput
+                            searchText={searchText}
+                            onSearch={(e) => onSearchChange(e)}
+                            onClear={() => {
+                              setSearchText('');
+                            }}
+                          />
+                        </div>
                       </div>
                     </div>
                     <p className="text-slate-500 dark:text-aa-200 purple:text-pt-200 text-sm tracking-sm">
