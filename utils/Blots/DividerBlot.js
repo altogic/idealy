a16c5b982cd2917c4426f7d3634333706e7efea4
Enes Malik Ozer <enes@altogic.com>
2023-04-18 10:58:57 +0300

    🎨 style(globals.css): change hr selector to .ql-divider 🔨 refactor(DividerBlot.js): add className property to DividerBlot class

--- utils/Blots/DividerBlot.js
+++ utils/Blots/DividerBlot.js
@@ -3,3 +3,4 @@ import { BlockEmbed } from './Blots';
 export default class DividerBlot extends BlockEmbed {}
 DividerBlot.blotName = 'divider';
 DividerBlot.tagName = 'hr';
+DividerBlot.className = 'ql-divider';
