6cecbf9ef7492096c366327f3b1d915ee37642f8
Enes Malik Ozer <enes@altogic.com>
2023-04-18 15:06:12 +0300

    🔨 refactor(icons): convert functional components to named function components

--- components/icons/LinkedIn.jsx
+++ components/icons/LinkedIn.jsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
-const SvgLinkedIn = (props) => (
-  <svg
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    {...props}>
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M3.42 2A1.42 1.42 0 0 0 2 3.42v17.16c0 .784.636 1.42 1.42 1.42h17.16A1.42 1.42 0 0 0 22 20.58V3.42A1.42 1.42 0 0 0 20.58 2H3.42Zm3.07 6.211a1.735 1.735 0 1 0 0-3.47 1.735 1.735 0 0 0 0 3.47Zm3.323 1.282h2.875v1.317s.78-1.56 2.904-1.56c1.894 0 3.463.933 3.463 3.777v5.996h-2.98v-5.27c0-1.677-.896-1.862-1.578-1.862-1.417 0-1.66 1.222-1.66 2.081v5.051H9.814v-9.53Zm-1.811 0H4.977v9.53h3.025v-9.53Z"
-      fill="currentColor"
-    />
-  </svg>
-);
+
+function SvgLinkedIn(props) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3.42 2A1.42 1.42 0 0 0 2 3.42v17.16c0 .784.636 1.42 1.42 1.42h17.16A1.42 1.42 0 0 0 22 20.58V3.42A1.42 1.42 0 0 0 20.58 2H3.42Zm3.07 6.211a1.735 1.735 0 1 0 0-3.47 1.735 1.735 0 0 0 0 3.47Zm3.323 1.282h2.875v1.317s.78-1.56 2.904-1.56c1.894 0 3.463.933 3.463 3.777v5.996h-2.98v-5.27c0-1.677-.896-1.862-1.578-1.862-1.417 0-1.66 1.222-1.66 2.081v5.051H9.814v-9.53Zm-1.811 0H4.977v9.53h3.025v-9.53Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
 export default SvgLinkedIn;
