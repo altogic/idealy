9d1024ae308b25695b5f6dff1d7c56e6e29db440
Enes Malik Ozer <enes@altogic.com>
2023-04-19 13:09:32 +0300

    🎨 style(AnnouncementSkeleton.jsx): change skeleton colors to match theme colors

--- components/AnnouncementSkeleton.jsx
+++ components/AnnouncementSkeleton.jsx
@@ -8,35 +8,35 @@ export default function AnnouncementSkeleton() {
         <div className="grow flex flex-col">
           <div className="flex justify-between items-center gap-2 divide-x divide-slate-200 dark:divide-aa-600 purple:divide-pt-600">
             <div className=" h-6 w-[44rem]">
-              <div className="animate-pulse rounded-md bg-gray-200 w-3/4 h-full" />
+              <div className="animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400 w-3/4 h-full" />
             </div>
 
             <div className="hidden md:block">
-              <div className="h-4 w-20 animate-pulse rounded-md bg-gray-200 ml-2" />
+              <div className="h-4 w-20 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400 ml-2" />
             </div>
             <div className="hidden md:block">
               <div className="flex flex-wrap items-center justify-center gap-2 ml-auto max-w-md pl-2">
-                <div className="h-4 w-24 animate-pulse rounded-md bg-gray-200" />
-                <div className="h-4 w-16 animate-pulse rounded-md bg-gray-200" />
+                <div className="h-4 w-24 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
+                <div className="h-4 w-16 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
               </div>
             </div>
             <div className="hidden md:block">
               <div className="pl-2">
                 <div className="flex items-center gap-2">
-                  <div className="h-6 w-6 animate-pulse rounded-full bg-gray-200" />
-                  <div className="h-6 w-6 animate-pulse rounded-full bg-gray-200" />
-                  <div className="h-6 w-6 animate-pulse rounded-full bg-gray-200" />
+                  <div className="h-6 w-6 animate-pulse rounded-full bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
+                  <div className="h-6 w-6 animate-pulse rounded-full bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
+                  <div className="h-6 w-6 animate-pulse rounded-full bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
                 </div>
               </div>
             </div>
             <div className="hidden md:block">
               <div className="flex items-center px-2 gap-2">
-                <div className="h-6 w-6 animate-pulse rounded-md bg-gray-200" />
-                <div className="h-6 w-6 animate-pulse rounded-md bg-gray-200" />
+                <div className="h-6 w-6 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
+                <div className="h-6 w-6 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
               </div>
             </div>
           </div>
-          <div className="mt-2 mb-8 h-4 w-32 animate-pulse rounded-md bg-gray-200" />
+          <div className="mt-2 mb-8 h-4 w-32 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400" />
           <div className="max-w-full mb-4 space-y-4">
             {Array.from({ length: 7 }, () => Math.floor(Math.random() * 21) + 80).map(
               (width, index) => (
@@ -45,7 +45,7 @@ export default function AnnouncementSkeleton() {
                   style={{
                     '--announcement-width': `${width}%`
                   }}
-                  className="w-[var(--announcement-width)] h-4 animate-pulse rounded-md bg-gray-200"
+                  className="w-[var(--announcement-width)] h-4 animate-pulse rounded-md bg-slate-200 dark:bg-aa-400 purple:bg-pt-400"
                 />
               )
             )}
