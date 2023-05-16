200ecc756847857fc4b8d871ba3c176f4895c8fd
Enes Malik Ozer <enes@altogic.com>
2023-05-16 20:18:54 +0300

    🎨 style(IdeaStatus.jsx, IdeaTopics.jsx): refactor code to use IdeaPreview component instead of duplicating code

--- layouts/create-company/IdeaStatus.jsx
+++ layouts/create-company/IdeaStatus.jsx
@@ -1,24 +1,19 @@
 import BaseListBox from '@/components/BaseListBox';
-import StatusBadge from '@/components/StatusBadge';
-import TopicBadges from '@/components/TopicBadges';
+import IdeaPreview from '@/components/IdeaPreview';
 import { companyActions } from '@/redux/company/companySlice';
 import _ from 'lodash';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function FourthWizard() {
-  const companyTopics = useSelector((state) => state.company.companyTopics);
-  const idea = useSelector((state) => state.company.idea);
-  const ideaDescription = useSelector((state) => state.company.ideaDescription);
+  const status = useSelector((state) => state.company.ideaStatus);
   const statuses = useSelector((state) => state.topic.statuses);
-  const [status, setStatus] = useState();
+
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(companyActions.setIdeaStatus(status));
-  }, [status]);
+
   useEffect(() => {
     if (!_.isNil(statuses)) {
-      setStatus(statuses[0]);
+      dispatch(companyActions.setIdeaStatus(statuses[0]));
     }
   }, [statuses]);
   return (
@@ -34,25 +29,14 @@ export default function FourthWizard() {
       <div className="space-y-6">
         <BaseListBox
           value={status}
-          onChange={setStatus}
+          onChange={(value) => dispatch(companyActions.setIdeaStatus(value))}
           options={statuses}
           field="name"
           label={status?.name}
           type="status"
           size="full"
         />
-        <div className="bg-white py-8 px-6 border border-slate-200 rounded-lg">
-          <h2 className="text-slate-800 mb-2 text-base font-semibold tracking-sm">{idea}</h2>
-          <p className="text-slate-500 mb-5 text-sm tracking-sm">{ideaDescription}</p>
-          <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-4">
-            <div className="flex flex-wrap items-center gap-4">
-              {companyTopics.map((topic) => (
-                <TopicBadges key={topic._id} badgeName={topic.name} />
-              ))}
-            </div>
-            {status && <StatusBadge name={status?.name} color={status?.color} />}
-          </div>
-        </div>
+        <IdeaPreview />
       </div>
     </>
   );
